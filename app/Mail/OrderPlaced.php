2389af8621ca913b8a8c4ffc23d7911fbf0e04bb
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-16 23:07:16 +0530

    Update OrderPlaced.php

--- app/Mail/OrderPlaced.php
+++ app/Mail/OrderPlaced.php
@@ -50,8 +50,9 @@ class OrderPlaced extends Mailable
         $restaurant_name = $order->branch->name;
        
         $delivery_man_name = $order->delivery_man->f_name.' '.$order->delivery_man->l_name;
-        print_r($delivery_man_name); die();
+        // print_r($delivery_man_name); die();
         $local = $order->customer->language_code ?? 'en';
+        print_r($local); die();
 
         $content = [
             'title' => $data->title,
