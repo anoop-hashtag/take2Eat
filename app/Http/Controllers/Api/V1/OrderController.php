e4b8d286908f042f0167e2cc8f31e106636d0b2e
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-16 15:00:56 +0530

    test

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -364,6 +364,7 @@ class OrderController extends Controller
 
                 //send email
                 $emailServices = Helpers::get_business_settings('mail_config');
+                echo $emailServices; die();
                 $order_mail_status = Helpers::get_business_settings('place_order_mail_status_user');
                 if (isset($emailServices['status']) && $emailServices['status'] == 1 && $order_mail_status == 1 && (bool)auth('api')->user()) {
                     Mail::to(auth('api')->user()->email)->send(new \App\Mail\OrderPlaced($order_id));
