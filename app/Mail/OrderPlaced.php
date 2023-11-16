5b49f781a391cb7dd0f38667790ab3f12a240e2a
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-16 17:40:25 +0530

    Update OrderPlaced.php

--- app/Mail/OrderPlaced.php
+++ app/Mail/OrderPlaced.php
@@ -15,36 +15,29 @@ class OrderPlaced extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
     protected $order_id;
 
     public function __construct($order_id)
     {
         $this->order_id = $order_id;
-        
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
         $order_id = $this->order_id;
-        $order=Order::where('id', $order_id)->first();
+        $order = Order::findOrFail($order_id); // Use findOrFail to throw an exception if the order is not found
         $company_name = BusinessSetting::where('key', 'restaurant_name')->first()->value;
-        $data=EmailTemplate::with('translations')->where('type','user')->where('email_type', 'new_order')->first();
-        $template=$data?$data->email_template:3;
-        $user_name = $order->customer->f_name.' '.$order->customer->l_name;
-        $restaurant_name = $order->branch->name;
-        $delivery_man_name = $order->delivery_man?->f_name.' '.$order->delivery_man?->l_name;
 
+        // Retrieve the email template data
+        $data = EmailTemplate::where('type', 'user')->where('email_type', 'new_order')->first();
+
+        // If no template is found, you might want to handle this case
+
+        $template = $data ? $data->email_template : 3;
         $local = $order->customer->language_code ?? 'en';
+
+        // Get translation data
+        $translations = $data ? $data->translations : [];
 
         $content = [
             'title' => $data->title,
@@ -53,20 +46,30 @@ class OrderPlaced extends Mailable
             'copyright_text' => $data->copyright_text
         ];
 
-        if ($local != 'en'){
-            if (isset($data->translations)){
-                foreach ($data->translations as $translation){
-                    if ($local == $translation->locale){
-                        $content[$translation->key] = $translation->value;
-                    }
+        // Check if translations exist and match the user's locale
+        if ($local != 'en' && !empty($translations)) {
+            foreach ($translations as $translation) {
+                if ($local == $translation->locale) {
+                    $content[$translation->key] = $translation->value;
                 }
             }
         }
 
-        $title = Helpers::text_variable_data_format( value:$data['title']??'',user_name:$user_name??'',restaurant_name:$restaurant_name??'',delivery_man_name:$delivery_man_name??'',order_id:$order_id??'');
-        $body = Helpers::text_variable_data_format( value:$data['body']??'',user_name:$user_name??'',restaurant_name:$restaurant_name??'',delivery_man_name:$delivery_man_name??'',order_id:$order_id??'');
-        $footer_text = Helpers::text_variable_data_format( value:$data['footer_text']??'',user_name:$user_name??'',restaurant_name:$restaurant_name??'',delivery_man_name:$delivery_man_name??'',order_id:$order_id??'');
-        $copyright_text = Helpers::text_variable_data_format( value:$data['copyright_text']??'',user_name:$user_name??'',restaurant_name:$restaurant_name??'',delivery_man_name:$delivery_man_name??'',order_id:$order_id??'');
-        return $this->subject(translate('Order_Place_Mail'))->view('email-templates.new-email-format-'.$template, ['company_name'=>$company_name,'data'=>$data,'title'=>$title,'body'=>$body,'footer_text'=>$footer_text,'copyright_text'=>$copyright_text,'order'=>$order]);
+        // Use the data directly in the text_variable_data_format method
+        $title = Helpers::text_variable_data_format($content['title'], $order);
+        $body = Helpers::text_variable_data_format($content['body'], $order);
+        $footer_text = Helpers::text_variable_data_format($content['footer_text'], $order);
+        $copyright_text = Helpers::text_variable_data_format($content['copyright_text'], $order);
+
+        return $this->subject(translate('Order_Place_Mail'))
+            ->view('email-templates.new-email-format-' . $template, [
+                'company_name' => $company_name,
+                'data' => $data,
+                'title' => $title,
+                'body' => $body,
+                'footer_text' => $footer_text,
+                'copyright_text' => $copyright_text,
+                'order' => $order
+            ]);
     }
 }
