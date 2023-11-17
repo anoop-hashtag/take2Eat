ec080c1f7ef42f44c48fd601d84ba1b6a25bab4e
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-17 13:54:04 +0530

    Update OrderPlaced.php

--- app/Mail/OrderPlaced.php
+++ app/Mail/OrderPlaced.php
@@ -41,7 +41,8 @@ class OrderPlaced extends Mailable
         // echo '<pre>'; print_r($order); die();
         $company_name = BusinessSetting::where('key', 'restaurant_name')->first()->value;
         //  print_r($company_name); die();
-        $data=EmailTemplate::with('')->where('type','user')->where('email_type', 'new_order')->first();
+        $data = EmailTemplate::where('type', 'user')->where('email_type', 'new_order')->first();
+
         // $data= EmailTemplate::with('translations')->where('type','user')->where('email_type', 'forget_password')->first();
         // print_r($data); die();
         $template=($data)?$data->email_template:"3";
