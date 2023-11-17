09e87ce47690c21cbb6d33886c455b560f5b08e6
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-17 14:12:32 +0530

    Update EmailTemplate.php

--- app/Models/EmailTemplate.php
+++ app/Models/EmailTemplate.php
@@ -16,72 +16,72 @@ class EmailTemplate extends Model
         return $this->morphMany(Translation::class, 'translationable');
     }
 
-//    public function getTitleAttribute($value){
-//        if (count($this->translations) > 0) {
-//            foreach ($this->translations as $translation) {
-//                if ($translation['key'] == 'title') {
-//                    return $translation['value'];
-//                }
-//            }
-//        }
-//
-//        return $value;
-//    }
-//
-//    public function getBodyAttribute($value){
-//        if (count($this->translations) > 0) {
-//            foreach ($this->translations as $translation) {
-//                if ($translation['key'] == 'body') {
-//                    return $translation['value'];
-//                }
-//            }
-//        }
-//
-//        return $value;
-//    }
-//
-//    public function getButtonNameAttribute($value){
-//        if (count($this->translations) > 0) {
-//            foreach ($this->translations as $translation) {
-//                if ($translation['key'] == 'button_name') {
-//                    return $translation['value'];
-//                }
-//            }
-//        }
-//
-//        return $value;
-//    }
-//
-//    public function getFooterTextAttribute($value){
-//        if (count($this->translations) > 0) {
-//            foreach ($this->translations as $translation) {
-//                if ($translation['key'] == 'footer_text') {
-//                    return $translation['value'];
-//                }
-//            }
-//        }
-//
-//        return $value;
-//    }
-//
-//    public function getCopyrightTextAttribute($value){
-//        if (count($this->translations) > 0) {
-//            foreach ($this->translations as $translation) {
-//                if ($translation['key'] == 'copyright_text') {
-//                    return $translation['value'];
-//                }
-//            }
-//        }
-//
-//        return $value;
-//    }
-//
-//    protected static function booted()
-//    {
-//        static::addGlobalScope('translate', function (Builder $builder) {
-//            $builder->with(['translations' => function ($query) {
-//                return $query->where('locale', app()->getLocale());
-//            }]);
-//        });
-//    }
+   public function getTitleAttribute($value){
+       if (count($this->translations) > 0) {
+           foreach ($this->translations as $translation) {
+               if ($translation['key'] == 'title') {
+                   return $translation['value'];
+               }
+           }
+       }
+
+       return $value;
+   }
+
+   public function getBodyAttribute($value){
+       if (count($this->translations) > 0) {
+           foreach ($this->translations as $translation) {
+               if ($translation['key'] == 'body') {
+                   return $translation['value'];
+               }
+           }
+       }
+
+       return $value;
+   }
+
+   public function getButtonNameAttribute($value){
+       if (count($this->translations) > 0) {
+           foreach ($this->translations as $translation) {
+               if ($translation['key'] == 'button_name') {
+                   return $translation['value'];
+               }
+           }
+       }
+
+       return $value;
+   }
+
+   public function getFooterTextAttribute($value){
+       if (count($this->translations) > 0) {
+           foreach ($this->translations as $translation) {
+               if ($translation['key'] == 'footer_text') {
+                   return $translation['value'];
+               }
+           }
+       }
+
+       return $value;
+   }
+
+   public function getCopyrightTextAttribute($value){
+       if (count($this->translations) > 0) {
+           foreach ($this->translations as $translation) {
+               if ($translation['key'] == 'copyright_text') {
+                   return $translation['value'];
+               }
+           }
+       }
+
+       return $value;
+   }
+
+   protected static function booted()
+   {
+       static::addGlobalScope('translate', function (Builder $builder) {
+           $builder->with(['translations' => function ($query) {
+               return $query->where('locale', app()->getLocale());
+           }]);
+       });
+   }
 }
