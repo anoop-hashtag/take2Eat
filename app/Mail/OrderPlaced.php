b26fca3b19687ce786f21f159d7b4ca1058a342e
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-17 10:39:58 +0530

    email test

--- app/Mail/OrderPlaced.php
+++ app/Mail/OrderPlaced.php
@@ -86,8 +86,8 @@ class OrderPlaced extends Mailable
         // $template_data = $this->subject(translate('Customer_Password_Reset_mail'))->view('email-templates.new-email-format-'.$template, ['company_name'=>$company_name,'data'=>$data,'title'=>$title,'body'=>$body,'footer_text'=>$footer_text,'copyright_text'=>$copyright_text,'url'=>"test.php", 'code'=>'678']);
 
         
-        return  $this->subject(translate('Order_Place_Mail'))->view('email-templates.new-email-format-'.$template, ['company_name'=>$company_name,'data'=>$data,'title'=>$title,'body'=>$body,'footer_text'=>$footer_text,'copyright_text'=>$copyright_text,'order'=>$order,'url'=>$url]);
-        // echo '<pre>';print_r($template_data); die();
+        $template_data =  $this->subject(translate('Order_Place_Mail'))->view('email-templates.new-email-format-'.$template, ['company_name'=>$company_name,'data'=>$data,'title'=>$title,'body'=>$body,'footer_text'=>$footer_text,'copyright_text'=>$copyright_text,'order'=>$order,'url'=>$url]);
+         echo '<pre>';print_r($template_data); die();
        
     }
 }
