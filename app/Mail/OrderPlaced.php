4d78d3d5c32ab369140bdbc8532f9996ef5cd95f
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-17 13:01:03 +0530

    Update OrderPlaced.php

--- app/Mail/OrderPlaced.php
+++ app/Mail/OrderPlaced.php
@@ -36,6 +36,9 @@ class OrderPlaced extends Mailable
      */
     public function build()
     {
+        $order_id = $this->order_id;
+        // print_r($order_id); die();
+        $order=Order::where('id', $order_id)->first();
         $code = $this->token;
        // return $this->view('email-templates.customer-password-reset', ['token' => $token]);
 
