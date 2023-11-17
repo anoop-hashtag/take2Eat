20a3c4ad88c2a6e481cdbbcb43d9f3ee0669af95
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-17 07:29:30 +0530

    Update OrderPlaced.php

--- app/Mail/OrderPlaced.php
+++ app/Mail/OrderPlaced.php
@@ -80,8 +80,10 @@ class OrderPlaced extends Mailable
         $body = Helpers::text_variable_data_format( value:$data['body']??'',user_name:$user_name??'',restaurant_name:$restaurant_name??'',delivery_man_name:$delivery_man_name??'',order_id:$order_id??'');
        
         $footer_text = Helpers::text_variable_data_format( value:$data['footer_text']??'',user_name:$user_name??'',restaurant_name:$restaurant_name??'',delivery_man_name:$delivery_man_name??'',order_id:$order_id??'');
-        echo '<pre>';print_r($footer_text); die();
+       
         $copyright_text = Helpers::text_variable_data_format( value:$data['copyright_text']??'',user_name:$user_name??'',restaurant_name:$restaurant_name??'',delivery_man_name:$delivery_man_name??'',order_id:$order_id??'');
+        echo '<pre>';print_r($copyright_text); die();
         return $this->subject(translate('Order_Place_Mail'))->view('email-templates.new-email-format-'.$template, ['company_name'=>$company_name,'data'=>$data,'title'=>$title,'body'=>$body,'footer_text'=>$footer_text,'copyright_text'=>$copyright_text,'order'=>$order]);
+       
     }
 }
