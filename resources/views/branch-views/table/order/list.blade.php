fd4f62e333bc308389d9cbb15c0defe9e019a5ae
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-24 09:34:57 +0530

    Date Format correct For Branch Order

--- resources/views/branch-views/table/order/list.blade.php
+++ resources/views/branch-views/table/order/list.blade.php
@@ -36,7 +36,7 @@
                                     <label class="text-dark">{{ translate('Start Date') }}</label>
                                     <input type="text" name="from"
                                            value="{{$from}}"
-                                           id="from_date" autocomplete="off" placeholder="DD-MM-YY" class="form-control">
+                                           id="from_date" autocomplete="off" placeholder="DD-MM-YYYY" class="form-control">
                                 </div>
                             </div>
                             <div class="col-md-6 col-lg-4">
@@ -44,7 +44,7 @@
                                     <label class="text-dark">{{ translate('End Date') }}</label>
                                     <input type="text"
                                            value="{{$to}}"
-                                           name="to" id="to_date" placeholder="DD-MM-YY" class="form-control">
+                                           name="to" id="to_date" placeholder="DD-MM-YYYY" class="form-control">
                                 </div>
                             </div>
                             <div class="col-12 col-lg-4 d-flex gap-2">
