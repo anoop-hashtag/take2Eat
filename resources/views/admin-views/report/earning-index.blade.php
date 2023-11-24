7d1fc3f0d869a597cf5fd5734cd68c260be4de3f
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-24 11:32:57 +0530

    For Spealing correction

--- resources/views/admin-views/report/earning-index.blade.php
+++ resources/views/admin-views/report/earning-index.blade.php
@@ -240,99 +240,85 @@
             <div class="card-body">
                 <!-- Bar Chart -->
                 <div class="chartjs-custom" style="height: 360px">
-                    <canvas class="js-chart" data-hs-chartjs-options='{
+                    <canvas class="js-chart"
+                            data-hs-chartjs-options='{
                         "type": "line",
                         "data": {
-                            "labels": ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
-                            "datasets": [{
-                                "data": [{{$sold[1]}},{{$sold[2]}},{{$sold[3]}},{{$sold[4]}},{{$sold[5]}},{{$sold[6]}},{{$sold[7]}},{{$sold[8]}},{{$sold[9]}},{{$sold[10]}},{{$sold[11]}},{{$sold[12]}}],
-                                "backgroundColor": ["rgba(55, 125, 255, 0)", "rgba(255, 255, 255, 0)"],
-                                "borderColor": "green",
-                                "borderWidth": 2,
-                                "pointRadius": 0,
-                                "pointBorderColor": "#fff",
-                                "pointBackgroundColor": "green",
-                                "pointHoverRadius": 0,
-                                "hoverBorderColor": "#fff",
-                                "hoverBackgroundColor": "#377dff"
-                            },
-                            {
-                                "data": [{{$tax[1]}},{{$tax[2]}},{{$tax[3]}},{{$tax[4]}},{{$tax[5]}},{{$tax[6]}},{{$tax[7]}},{{$tax[8]}},{{$tax[9]}},{{$tax[10]}},{{$tax[11]}},{{$tax[12]}}],
-                                "backgroundColor": ["rgba(0, 201, 219, 0)", "rgba(255, 255, 255, 0)"],
-                                "borderColor": "#ec9a3c",
-                                "borderWidth": 2,
-                                "pointRadius": 0,
-                                "pointBorderColor": "#fff",
-                                "pointBackgroundColor": "#ec9a3c",
-                                "pointHoverRadius": 0,
-                                "hoverBorderColor": "#fff",
-                                "hoverBackgroundColor": "#00c9db"
-                            }]
+                           "labels": ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+                           "datasets": [{
+                            "data": [{{$sold[1]}},{{$sold[2]}},{{$sold[3]}},{{$sold[4]}},{{$sold[5]}},{{$sold[6]}},{{$sold[7]}},{{$sold[8]}},{{$sold[9]}},{{$sold[10]}},{{$sold[11]}},{{$sold[12]}}],
+                            "backgroundColor": ["rgba(55, 125, 255, 0)", "rgba(255, 255, 255, 0)"],
+                            "borderColor": "green",
+                            "borderWidth": 2,
+                            "pointRadius": 0,
+                            "pointBorderColor": "#fff",
+                            "pointBackgroundColor": "green",
+                            "pointHoverRadius": 0,
+                            "hoverBorderColor": "#fff",
+                            "hoverBackgroundColor": "#377dff"
+                          },
+                          {
+                            "data": [{{$tax[1]}},{{$tax[2]}},{{$tax[3]}},{{$tax[4]}},{{$tax[5]}},{{$tax[6]}},{{$tax[7]}},{{$tax[8]}},{{$tax[9]}},{{$tax[10]}},{{$tax[11]}},{{$tax[12]}}],
+                            "backgroundColor": ["rgba(0, 201, 219, 0)", "rgba(255, 255, 255, 0)"],
+                            "borderColor": "#ec9a3c",
+                            "borderWidth": 2,
+                            "pointRadius": 0,
+                            "pointBorderColor": "#fff",
+                            "pointBackgroundColor": "#ec9a3c",
+                            "pointHoverRadius": 0,
+                            "hoverBorderColor": "#fff",
+                            "hoverBackgroundColor": "#00c9db"
+                          }]
                         },
                         "options": {
-                            "gradientPosition": {"y1": 200},
-                            "scales": {
-                                "yAxes": [{
-                                    "gridLines": {
-                                        "color": "#e7eaf3",
-                                        "drawBorder": false,
-                                        "zeroLineColor": "#e7eaf3"
-                                    },
-                                    "ticks": {
-                                        "min": 0,
-                                        "max": {{\App\CentralLogics\Helpers::max_earning()}},
-                                        "stepSize": {{round(\App\CentralLogics\Helpers::max_earning()/5)}},
-                                        "fontColor": "#97a4af",
-                                        "fontFamily": "Open Sans, sans-serif",
-                                        "padding": 10,
-                                        "prefix": " {{\App\CentralLogics\Helpers::currency_symbol()}} ",
-                                        "callback": function (value, index, values) {
-                                            return value.toFixed(2); // Display values with two decimal places
-                                        }
-                                    }
-                                }],
-                                "xAxes": [{
-                                    "gridLines": {
-                                        "display": false,
-                                        "drawBorder": false
-                                    },
-                                    "ticks": {
-                                        "fontSize": 12,
-                                        "fontColor": "#97a4af",
-                                        "fontFamily": "Open Sans, sans-serif",
-                                        "padding": 5
-                                    }
-                                }]
-                            },
-                            "tooltips": {
-                                "prefix": "",
-                                "postfix": "",
-                                "hasIndicator": true,
-                                "mode": "index",
-                                "intersect": false,
-                                "lineMode": true,
-                                "lineWithLineColor": "rgba(19, 33, 68, 0.075)",
-                                "callbacks": {
-                                    "label": function (tooltipItem, data) {
-                                        return data.datasets[tooltipItem.datasetIndex].label + ': ' + tooltipItem.yLabel.toFixed(2);
-                                    },
-                                    "labelColor": function (tooltipItem, chart) {
-                                        return {
-                                            borderColor: 'rgba(255, 255, 255, 0.8)',
-                                            backgroundColor: 'rgba(255, 255, 255, 0.8)'
-                                        };
-                                    }
+                          "gradientPosition": {"y1": 200},
+                           "scales": {
+                              "yAxes": [{
+                                "gridLines": {
+                                  "color": "#e7eaf3",
+                                  "drawBorder": false,
+                                  "zeroLineColor": "#e7eaf3"
+                                },
+                                "ticks": {
+                                  "min": 0,
+                                  "max": {{\App\CentralLogics\Helpers::max_earning()}},
+                                  "stepSize": {{round(\App\CentralLogics\Helpers::max_earning()/5)}},
+                                  "fontColor": "#97a4af",
+                                  "fontFamily": "Open Sans, sans-serif",
+                                  "padding": 10,
+                                  "prefix": " {{\App\CentralLogics\Helpers::currency_symbol()}} "
                                 }
-                            },
-                            "hover": {
-                                "mode": "nearest",
-                                "intersect": true
-                            }
+                              }],
+                              "xAxes": [{
+                                "gridLines": {
+                                  "display": false,
+                                  "drawBorder": false
+                                },
+                                "ticks": {
+                                  "fontSize": 12,
+                                  "fontColor": "#97a4af",
+                                  "fontFamily": "Open Sans, sans-serif",
+                                  "padding": 5
+                                }
+                              }]
+                          },
+                          "tooltips": {
+                            "prefix": "",
+                            "postfix": "",
+                            "hasIndicator": true,
+                            "mode": "index",
+                            "intersect": false,
+                            "lineMode": true,
+                            "lineWithLineColor": "rgba(19, 33, 68, 0.075)"
+                          },
+                          "hover": {
+                            "mode": "nearest",
+                            "intersect": true
+                          }
                         }
-                    }'>
+                      }'>
                     </canvas>
                 </div>
-                
                 <!-- End Bar Chart -->
             </div>
             <!-- End Body -->
