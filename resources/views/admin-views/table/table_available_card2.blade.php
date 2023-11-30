8b1018c313f14eb1f6f21bdde3654d73c4e34611
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-11-30 18:50:44 +0530

    Update table_available_card2.blade.php

--- resources/views/admin-views/table/table_available_card2.blade.php
+++ resources/views/admin-views/table/table_available_card2.blade.php
@@ -14,21 +14,43 @@
             </div>
         </div>
         <div class="table_hover-menu px-3">
-            <h3 class="mb-3">{{ translate('Table - ') }}{{ $table['number'] }}</h3>
-            @if(($table['order'] != null))
-                @foreach($table['order'] as $order)
-                    <div class="fz-14 mb-1">{{ translate('order id') }}: <strong>{{ $order['id'] }}</strong></div>
-                @endforeach
-            @else
-                <div class="fz-14 mb-1">{{ translate('current status') }} - <strong>{{ translate('empty') }}</strong></div>
-                <div class="fz-14 mb-1">{{ translate('any reservation') }} - <strong>{{ translate('N/A') }}</strong></div>
-            @endif
-{{-- next release--}}
-{{--            <div class="d-flex flex-wrap gap-2 mt-3">--}}
-{{--                <a href="#" data-dismiss="modal" class="btn btn-outline-primary text-nowrap stopPropagation" data-toggle="modal" data-target="#reservationModal"><i class="tio-alarm-alert"></i> {{ translate('Create_Reservation') }}</a>--}}
-{{--                <a href="#" class="btn btn-primary text-nowrap">{{ translate('Place_Order') }}</a>--}}
-{{--            </div>--}}
+            
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="text-center position-relative px-4 py-5">
+                        <h3 class="mb-3">{{ translate('Table - ') }}{{ $table['number'] }}</h3>
+                        @if(($table['order'] != null))
+                            @foreach($table['order'] as $order)
+                            <table class="order-table">
+                                <tr>
+                                    <th>{{ translate('Order ID') }}</th>
+                                    <td>
+                                        <span class="order-id-container">
+                                            <strong>{{ $order['id'] }}</strong>
+                                        </span>
+                                    </td>
+                                </tr>
+                                <!-- Add more rows if needed for additional information -->
+                            </table>
+                            @endforeach
+                        @else
+                            <div class="fz-14 mb-1">{{ translate('current status') }} - <strong>{{ translate('empty') }}</strong></div>
+                            <div class="fz-14 mb-1">{{ translate('any reservation') }} - <strong>{{ translate('N/A') }}</strong></div>
+                        @endif
+            {{-- next release--}}
+            {{--            <div class="d-flex flex-wrap gap-2 mt-3">--}}
+            {{--                <a href="#" data-dismiss="modal" class="btn btn-outline-primary text-nowrap stopPropagation" data-toggle="modal" data-target="#reservationModal"><i class="tio-alarm-alert"></i> {{ translate('Create_Reservation') }}</a>--}}
+            {{--                <a href="#" class="btn btn-primary text-nowrap">{{ translate('Place_Order') }}</a>--}}
+            {{--            </div>--}}
+            
+                        <div class="d-flex mt-5 mx-lg-5">
+                            <a href="#" class="btn btn-outline-primary w-100 text-nowrap" data-dismiss="modal" data-toggle="modal" data-target="#reservationModal"><i class="tio-alarm-alert"></i> {{ translate('Create_Reservation') }}</a>
+                        </div>
+                    </div>
+                </div>
+              </div>
         </div>
+        
     </div>
 @endforeach
 @else
@@ -79,57 +101,50 @@
 <div class="modal fade" id="reservationModal" tabindex="-1" role="dialog" aria-labelledby="reservationModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-        <div class="position-relative px-4 py-5">
-            <button type="button" class="close text-primary position-absolute right-2 top-2 fz-24" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <form action="#">
-                <div class="text-center">
-                    <h3 class="mb-3">{{ translate('Table# - D2 ') }}</h3>
-                    <div class="fz-14 mb-1">{{ translate('current status') }} - <strong>{{ translate('Available') }}</strong></div>
-                </div>
-
-                <div class="mb-4 mt-5">
-                    <label for="table_no">{{ translate('Table_No') }}</label>
-                    <select name="table_no" id="table_no" class="custom-select">
-                        <option value="#" selected disabled>{{ translate('Select_Tables') }}</option>
-                        <option value="#">{{ translate('D1') }}</option>
-                        <option value="#">{{ translate('D2') }}</option>
-                        <option value="#">{{ translate('D3') }}</option>
-                        <option value="#">{{ translate('D4') }}</option>
-                        <option value="#">{{ translate('D5') }}</option>
-                        <option value="#">{{ translate('D6') }}</option>
-                        <option value="#">{{ translate('D7') }}</option>
-                    </select>
-                </div>
-
-                <div class="mb-4 mt-5">
-                    <label for="reservation_time">{{ translate('Reservation_Time') }}</label>
-                    <input type="date" id="reservation_time" class="form-control">
-                </div>
-
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="mb-4">
-                            <label for="start_time">{{ translate('Start_Time') }}</label>
-                            <input type="time" id="start_time" class="form-control">
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-4">
-                            <label for="end_time">{{ translate('end_Time') }}</label>
-                            <input type="time" id="end_time" class="form-control">
-                        </div>
-                    </div>
-                </div>
-
-                <p class="text-primary text-center mt-3"> *  Sorry, There is already another reservation in this time </p>
-
-                <div class="d-flex justify-content-center mt-4">
-                    <button type="submit" class="btn btn-primary px-lg-5 text-nowrap">{{ translate('Book_Reservation') }}</button>
-                </div>
-            </form>
+        <div class="table_hover-menu px-3">
+            <h3 class="mb-3">{{ translate('Table - ') }}{{ $table['number'] }}</h3>
+            @if(($table['order'] != null))
+                @foreach($table['order'] as $order)
+                    <div class="fz-14 mb-1">{{ translate('order id') }}: <strong>{{ $order['id'] }}</strong></div>
+                @endforeach
+            @else
+                <div class="fz-14 mb-1">{{ translate('current status') }} - <strong>{{ translate('empty') }}</strong></div>
+                <div class="fz-14 mb-1">{{ translate('any reservation') }} - <strong>{{ translate('N/A') }}</strong></div>
+            @endif
+{{-- next release--}}
+{{--            <div class="d-flex flex-wrap gap-2 mt-3">--}}
+{{--                <a href="#" data-dismiss="modal" class="btn btn-outline-primary text-nowrap stopPropagation" data-toggle="modal" data-target="#reservationModal"><i class="tio-alarm-alert"></i> {{ translate('Create_Reservation') }}</a>--}}
+{{--                <a href="#" class="btn btn-primary text-nowrap">{{ translate('Place_Order') }}</a>--}}
+{{--            </div>--}}
         </div>
     </div>
   </div>
 </div>
+
+<style>
+    /* Add these styles to your CSS or style section */
+    .order-table {
+        width: 100%; /* Set the width of the table */
+        border-collapse: collapse; /* Collapse borders for a cleaner look */
+        margin-top: 10px; /* Adjust margin as needed */
+    }
+
+    .order-table th, .order-table td {
+        padding: 10px; /* Set padding for cells */
+        border: 1px solid #ddd; /* Add a border to cells for separation */
+        text-align: left; /* Align text to the left within cells */
+    }
+
+    .order-id-container {
+        background-color: #3498db; /* Set your desired background color */
+        color: #ffffff; /* Set your desired text color */
+        padding: 8px; /* Adjust padding as needed */
+        border-radius: 4px; /* Add border-radius for rounded corners */
+        display: inline-block; /* Make it inline-block to fit content */
+    }
+
+    .order-id-container strong {
+        font-weight: bold; /* Make the strong element bold */
+        color: #e74c3c; /* Set a different color for the strong element */
+    }
+</style>
