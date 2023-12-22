026f4db55bc141179dda9696876d8659303d04da
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2023-12-22 15:32:12 +0530

    Deliveryman update template Delivery Partner

--- resources/views/admin-views/business-settings/email-format-setting/partials/dm-email-template-setting-links.blade.php
+++ resources/views/admin-views/business-settings/email-format-setting/partials/dm-email-template-setting-links.blade.php
@@ -5,19 +5,19 @@
             <li class="nav-item">
                 <a class="nav-link {{ Request::is('admin/business-settings/email-setup/dm/registration') ? 'active' : '' }}"
                 href="{{ route('admin.business-settings.email-setup', ['dm','registration']) }}">
-                {{translate('New_Deliveryman_Registration')}}
+                {{translate('New_Delivery_Partner_Registration')}}
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link {{ Request::is('admin/business-settings/email-setup/dm/approve') ? 'active' : '' }}"
                 href="{{ route('admin.business-settings.email-setup', ['dm','approve']) }}">
-                {{translate('New_Deliveryman_Approval')}}
+                {{translate('New_Delivery_Partner_Approval')}}
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link {{ Request::is('admin/business-settings/email-setup/dm/deny') ? 'active' : '' }}"
                 href="{{ route('admin.business-settings.email-setup', ['dm','deny']) }}">
-                {{translate('New_Deliveryman_Rejection')}}
+                {{translate('New_Delivery_Partner_Rejection')}}
                 </a>
             </li>
         </ul>
--- resources/views/admin-views/business-settings/email-format-setting/partials/email-template-options.blade.php
+++ resources/views/admin-views/business-settings/email-format-setting/partials/email-template-options.blade.php
@@ -2,5 +2,5 @@
 {{--    <option value="admin" {{ Request::is('admin/business-settings/email-setup/admin*') ? 'selected' : '' }}>{{ translate('Admin_Mail_Templates') }}</option>--}}
 {{--    <option value="restaurant" {{ Request::is('admin/business-settings/email-setup/restaurant*') ? 'selected' : '' }}>{{ translate('Restaurant_Mail_Templates') }}</option>--}}
     <option value="user" {{ Request::is('admin/business-settings/email-setup/user*') ? 'selected' : '' }}>{{ translate('Customer_Mail_Templates') }}</option>
-    <option value="dm" {{ Request::is('admin/business-settings/email-setup/dm*') ? 'selected' : '' }}>{{ translate('Deliveryman_Mail_Templates') }}</option>
+    <option value="dm" {{ Request::is('admin/business-settings/email-setup/dm*') ? 'selected' : '' }}>{{ translate('Delivery_Partner_Mail_Templates') }}</option>
 </select>
