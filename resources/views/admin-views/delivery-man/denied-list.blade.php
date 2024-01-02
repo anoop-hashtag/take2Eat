22a71a24a1d61504c89dd070c5ab2f5b2c6d87be
SwetaHashtag <153063653+SwetaHashtag@users.noreply.github.com>
2024-01-02 18:14:42 +0530

    UI-responsive-search

--- resources/views/admin-views/delivery-man/denied-list.blade.php
+++ resources/views/admin-views/delivery-man/denied-list.blade.php
@@ -51,7 +51,7 @@
                         </div> --}}
                     </div>
 
-                    <div class="set_table">
+                    <div class="set_table responsive-ui new-ui">
                         <div class="table-responsive datatable_wrapper_row "  style="padding-right: 10px;">
                             <table id="datatable" class="table table-borderless table-thead-bordered table-nowrap table-align-middle card-table">
                                 <thead class="thead-light">
