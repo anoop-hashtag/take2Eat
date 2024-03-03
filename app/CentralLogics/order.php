e9afc510ded9b30b985e876bb70de706cf465e9c
sonuyadavkn <149492336+sonuyadavkn@users.noreply.github.com>
2024-03-03 22:46:45 +0530

    Update order.php

--- app/CentralLogics/order.php
+++ app/CentralLogics/order.php
@@ -13,11 +13,9 @@ class OrderLogic
 {
     public static function track_order($order_id)
     {
-        $datanew= Helpers::order_data_formatting(Order::with(['details', 'delivery_man.rating','order_partial_payments'])
+        return Helpers::order_data_formatting(Order::with(['details', 'delivery_man.rating','order_partial_payments'])
             ->where(['id' => $order_id])
             ->first(), false);
-            dd($datanew);
-
     }
 
     public static function place_order($customer_id, $email, $customer_info, $cart, $payment_method, $discount, $coupon_code = null)
