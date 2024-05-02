50dbf8190f3535061bb2cf53061751b2a19bad33
Shubham-Hashtag <shubham.singh2@hashtaglabs.in>
2024-05-02 12:26:29 +0530

    Update new-email-format-3.blade.php

--- resources/views/email-templates/new-email-format-3.blade.php
+++ resources/views/email-templates/new-email-format-3.blade.php
@@ -123,7 +123,7 @@
                                           <span>{{$detail['quantity']}}</span>
 
                                           <br/>
-                                          @if ($amount > 0)
+                                          @if (isset($detail['variation']) && count($addon_ids) > 0)
                                             <span class="">{{translate('price')}} :  </span>
                                             <span>{{\App\CentralLogics\Helpers::set_symbol($detail['price'])}}</span>
                                             <br/>
