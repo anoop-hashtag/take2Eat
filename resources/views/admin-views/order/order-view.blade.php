eee5c54f15ae59c55fae8592ab893c5b8e397e74
Shubham-Hashtag <shubham.singh2@hashtaglabs.in>
2024-05-08 10:33:43 +0530

    Uncomment auto reload
    Uncomment auto reload

--- resources/views/admin-views/order/order-view.blade.php
+++ resources/views/admin-views/order/order-view.blade.php
@@ -1288,11 +1288,11 @@
     </script>
     <script>
         // Function to reload the page
-        // function reloadPage() {
-        //     location.reload();
-        // }
+        function reloadPage() {
+            location.reload();
+        }
         
         // // Set timeout to reload the page every 10 seconds (10000 milliseconds)
-        // setTimeout(reloadPage, 10000);
+        setTimeout(reloadPage, 10000);
     </script>
 @endpush
