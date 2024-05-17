b01ef3f2dbc74e1e79e48d3cfdf22d5294d40ae6
SwetaHashtag <153063653+SwetaHashtag@users.noreply.github.com>
2024-05-17 14:11:40 +0530

    banner-list

--- resources/views/admin-views/banner/list.blade.php
+++ resources/views/admin-views/banner/list.blade.php
@@ -202,7 +202,7 @@
                     </div>
 
                     <!-- Table -->
-                    <div class="set_table">
+                    <div class="set_table banner-tbl">
                         <div class="table-responsive datatable_wrapper_row "  style="padding-right: 10px;">
                             <table id="datatable" class="table table-borderless table-thead-bordered table-nowrap table-align-middle card-table">
                                 <thead class="thead-light">
