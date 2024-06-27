44115489bd81259c6a10cd9d47ef5333319b2383
Shubham-Hashtag <shubham.singh2@hashtaglabs.in>
2024-06-27 12:57:42 +0530

    Add validation on return purchase
    Add validation on return purchase

--- resources/views/admin-views/return-purchase/add.blade.php
+++ resources/views/admin-views/return-purchase/add.blade.php
@@ -61,95 +61,106 @@
                     </div>
                 </form>
                 <!-- Table -->
-                <div class="set_table banner-tbl mt-4" >
-                    <div class="table-responsive datatable_wrapper_row">
-                        <table id="datatable"  class="table table-borderless table-thead-bordered table-nowrap table-align-middle card-table">
-                            <thead class="thead-light">
-                            <tr>
-                                <th><input type="checkbox" name="text" required></th>
-                                <th style="width: 35%;">{{translate('Item')}}</th>
-                                <th style="width: 15%;">{{translate('quantity')}}</th>
-                                <th style="width: 15%;">{{translate('quantity_type')}}</th>
-                                <th style="width: 15%;">{{translate('rate')}}</th>
-                                <th style="width: 15%;">{{translate('total')}}</th>
-                            </tr>
-                            </thead>
-                            @if (isset($purchaseIngredients))
-                                <form action="{{ route('admin.return-purchase.store') }}" method="post">
+                <form action="{{ route('admin.return-purchase.store') }}" method="post">
+                    <!-- for note -->
+                    @if (isset($vendor_id) && isset($invoice))
+                        <div class="row">
+                            <div class="col-sm-1"></div>
+                            <div class="col-sm-10">
+                                <div class="form-group">
+                                    <label class="input-label">{{translate('Note')}}</label>
+                                    <textarea name="note" class="form-control" placeholder="{{translate('Ex: ABC')}}" style="resize: none;"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    
+                    <div class="set_table banner-tbl mt-4" >
+                        <div class="table-responsive datatable_wrapper_row">
+                            <table id="datatable"  class="table table-borderless table-thead-bordered table-nowrap table-align-middle card-table">
+                                <thead class="thead-light">
+                                <tr>
+                                    <th><input type="checkbox" name="text"></th>
+                                    <th style="width: 35%;">{{translate('Item')}}</th>
+                                    <th style="width: 15%;">{{translate('quantity')}}</th>
+                                    <th style="width: 15%;">{{translate('quantity_type')}}</th>
+                                    <th style="width: 15%;">{{translate('rate')}}</th>
+                                    <th style="width: 15%;">{{translate('total')}}</th>
+                                </tr>
+                                </thead>
+                                @if (isset($purchaseIngredients))
+                                        <tbody>
+                                            @csrf
+                                            <input type="hidden" name="purchase_id" value="{{ $purchaseIngredients[0]->id }}" />
+                                            @foreach ($purchaseIngredients as $key => $purchaseIngredient)
+                                                <tr>
+                                                    <td>
+                                                        <input type="checkbox" name="return_ingredients[{{$key}}]" class="form-control" value="{{ $purchaseIngredient->purchases_ingredient_items_id }}">
+                                                    </td>
+                                                    <td>
+                                                        <select name="items[{{$key}}]" onchange="updateQtyType(this)" class="custom-select items">
+                                                            <option selected disabled>{{translate('select_item')}}</option>
+                                                            @foreach ($ingredients as $ingredient)
+                                                                @if ($ingredient->id == json_decode($purchaseIngredient->ingredient_details)->id)
+                                                                    <option value="{{ $ingredient->id }}" selected>{{ $ingredient->name }}</option>
+                                                                @endif
+                                                            @endforeach
+                                                        </select>
+                                                    </td>
+                                                    <td>
+                                                        <input type="number" name="quantitys[{{$key}}]" min="0" step="1" max="{{ $purchaseIngredient->quantity }}"  value="{{ $purchaseIngredient->quantity }}" onchange="calculateMax(this)" onkeyup="calculateTotal(this)"  class="form-control quantity qty" required>
+                                                        <input type="hidden" class="main_quantity" value="{{ $purchaseIngredient->quantity }}" />
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" class="form-control quantity_type qty-type" value="{{ json_decode($purchaseIngredient->ingredient_details)->quantity_type }}" readonly>
+                                                    </td>
+                                                    <td>
+                                                        <input type="number" class="form-control rate"  onkeyup="calculateTotal(this)"  value="{{ $purchaseIngredient->rate }}" readonly>
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" class="form-control total" value="{{ $purchaseIngredient->rate * $purchaseIngredient->quantity }}" readonly>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                    <div class="d-flex justify-content-end gap-3 m-4">
+                                        <button type="reset" id="reset" class="btn btn-secondary">{{translate('reset')}}</button>
+                                        <button type="submit" class="btn btn-primary">{{translate('return')}}</button>
+                                    </div>
+                                @else
                                     <tbody>
-                                        @csrf
-                                        <input type="hidden" name="purchase_id" value="{{ $purchaseIngredients[0]->id }}" />
-
-                                        @foreach ($purchaseIngredients as $key => $purchaseIngredient)
-                                            <tr>
-                                                <td>
-                                                    <input type="checkbox" name="return_ingredients[{{$key}}]" class="form-control" value="{{ $purchaseIngredient->purchases_ingredient_items_id }}">
-                                                </td>
-                                                <td>
-                                                    <select name="items[{{$key}}]" class="custom-select items">
-                                                        <option selected disabled>{{translate('select_item')}}</option>
-                                                        @foreach ($ingredients as $ingredient)
-                                                            <option value="{{ $ingredient->id }}"
-                                                                {{ $ingredient->id == json_decode($purchaseIngredient->ingredient_details)->id ? 'selected' : '' }}
-                                                            >{{ $ingredient->name }}</option>
-                                                        @endforeach
-                                                    </select>
-                                                </td>
-                                                <td>
-                                                    <input type="number" name="quantitys[{{$key}}]" class="form-control quantity" value="{{ $purchaseIngredient->quantity }}" required>
-                                                    <input type="hidden" class="main_quantity" value="{{ $purchaseIngredient->quantity }}" />
-                                                </td>
-                                                <td>
-                                                    <input type="text" class="form-control quantity_type" value="{{ json_decode($purchaseIngredient->ingredient_details)->quantity_type }}" readonly>
-                                                </td>
-                                                <td>
-                                                    <input type="number" class="form-control rate" value="{{ $purchaseIngredient->rate }}" readonly>
-                                                </td>
-                                                <td>
-                                                    <input type="text" class="form-control total" value="{{ $purchaseIngredient->rate * $purchaseIngredient->quantity }}" readonly>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-
+                                        <tr>
+                                            <td>
+                                                <input type="checkbox" name="text" class="form-control">
+                                            </td>
+                                            <td>
+                                                <select name="items[]" class="custom-select items">
+                                                    <option selected disabled>{{translate('select_item')}}</option>
+                                                    {{-- @foreach ($ingredients as $ingredient)
+                                                        <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
+                                                    @endforeach --}}
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <input type="number" name="quantitys[]" class="form-control quantity" required>
+                                            </td>
+                                            <td>
+                                                <input type="text" class="form-control quantity_type" readonly>
+                                            </td>
+                                            <td>
+                                                <input type="number" name="rates[]" class="form-control rate" required>
+                                            </td>
+                                            <td>
+                                                <input type="text" class="form-control total" required readonly>
+                                            </td>
+                                        </tr>
                                     </tbody>
                                 </table>
-                                <div class="d-flex justify-content-end gap-3 m-4">
-                                    <button type="reset" id="reset" class="btn btn-secondary">{{translate('reset')}}</button>
-                                    <button type="submit" class="btn btn-primary">{{translate('return')}}</button>
-                                </div>
-                            </form>
-                            @else
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <input type="checkbox" name="text" class="form-control">
-                                        </td>
-                                        <td>
-                                            <select name="items[]" class="custom-select items">
-                                                <option selected disabled>{{translate('select_item')}}</option>
-                                                {{-- @foreach ($ingredients as $ingredient)
-                                                    <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
-                                                @endforeach --}}
-                                            </select>
-                                        </td>
-                                        <td>
-                                            <input type="number" name="quantitys[]" class="form-control quantity" required>
-                                        </td>
-                                        <td>
-                                            <input type="text" class="form-control quantity_type" readonly>
-                                        </td>
-                                        <td>
-                                            <input type="number" name="rates[]" class="form-control rate" required>
-                                        </td>
-                                        <td>
-                                            <input type="text" class="form-control total" required readonly>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        @endif
+                            @endif
+                        </div>
                     </div>
-                </div>
+                </form>
                 <!-- End Table -->
                 
             </div>
@@ -186,6 +197,58 @@
                         alert(response.message);
                     }
                 }
+            });
+        });
+    </script>
+
+    <script>
+        function calculateMax(input) {
+            var enteredValue = parseInt(input.value);
+            var maxValue = parseInt(input.getAttribute('max'));
+    
+            if (isNaN(enteredValue) || !Number.isInteger(enteredValue)) {
+                input.value = '';
+                document.querySelector('.max-error').style.display = 'none';
+            } else if (enteredValue > maxValue) {
+                input.value = Math.floor(maxValue);
+                document.querySelector('.max-error').style.display = 'inline';
+            } else {
+                document.querySelector('.max-error').style.display = 'none';
+            }
+        }
+    
+        function updateQtyType(select) {
+            var selectedOption = select.options[select.selectedIndex];
+            var qtyTypInput = $(select).closest('tr').find('.qty-type'); 
+            var qtyTyp = $(selectedOption).data('qtytyp');
+            qtyTypInput.val(qtyTyp);
+        }
+    
+        function calculateTotal(input) {
+            var row = $(input).closest('tr'); 
+            var qty = parseFloat(row.find('.qty').val()) || 0; 
+            var rate = parseFloat(row.find('.rate').val()) || 0; 
+            var total = qty * rate;
+            if (isNaN(total) || !isFinite(total)) {
+                total = 0;
+            }
+            row.find('.total').val(total.toFixed(0));
+        }
+    
+        function showAlertOnZero(input) {
+            let quantity = parseFloat(input.value.trim());
+            if (isNaN(quantity) || quantity <= 0) {
+                alert("Quantity must be a positive number and cannot be 0. Please enter a valid quantity.");
+                input.value = '';
+            }
+        }
+    
+        document.addEventListener('DOMContentLoaded', function() {
+            const inputs = document.querySelectorAll('.qty');
+            inputs.forEach(function(input) {
+                input.addEventListener('keyup', function() {
+                    showAlertOnZero(input);
+                });
             });
         });
     </script>
